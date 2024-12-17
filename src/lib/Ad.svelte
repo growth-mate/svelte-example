a604224a7956f29619e08b3ea72261858f6aec50
lennart Czardybon <lennczar@gmail.com>
2024-12-17 17:02:22 +0100

    fix: check if component is mounted upon updating it

--- src/lib/Ad.svelte
+++ src/lib/Ad.svelte
@@ -1,40 +1,60 @@
+<script context="module" lang="ts">
+  declare global {
+    interface Window {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      growthmate: any;
+    }
+  }
+</script>
+
 <script lang="ts">
-	export let unitId: string;
-	export let format: string;
-	export let accountId: string | undefined = undefined;
-	export let network: string | undefined = undefined;
-	export let className: string | undefined = undefined;
+  export let unitId: string;
+  export let format: string;
+  export let accountId: string | undefined = undefined;
+  export let network: string | undefined = undefined;
+  export let className: string | undefined = undefined;
 
-	import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy } from "svelte";
 
-	$: {
-        void accountId, void network;
-		if ((window as any).growthmate !== undefined) (window as any).growthmate.register(unitId);
-	}
+  let mounted = false;
 
-	onMount(() => {
-		if ((window as any).growthmate !== undefined) (window as any).growthmate.register(unitId);
+  $: {
+    void accountId, void network;
+    if (mounted && window.growthmate !== undefined)
+      window.growthmate.register(unitId);
+  }
 
-		let script: HTMLScriptElement | null = document.querySelector("#gm-ad-script");
-		if (!script) {
-			script = document.createElement("script");
-			script.src = "https://cdn.growthmate.xyz/scripts/ad-unit-manager.react.js";
-			script.id = "gm-ad-script";
-			document.head.appendChild(script);
-		}
+  onMount(() => {
+    if (window.growthmate !== undefined) window.growthmate.register(unitId);
 
-		script.addEventListener("load", () => (window as any).growthmate.register(unitId));
-	});
+    let script: HTMLScriptElement | null =
+      document.querySelector("#gm-ad-script");
+    if (!script) {
+      script = document.createElement("script");
+      script.src =
+        "https://cdn.growthmate.xyz/scripts/ad-unit-manager.react.js";
+      script.id = "gm-ad-script";
+      document.head.appendChild(script);
+    }
 
-	onDestroy(() => {
-		(window as any).growthmate?.unregister(unitId);
-	});
+    script.addEventListener("load", () => window.growthmate.register(unitId));
+
+    mounted = true;
+  });
+
+  onDestroy(() => {
+    window.growthmate?.unregister(unitId);
+
+    mounted = false;
+  });
 </script>
 
 <a
-	class="gm-ad-unit {className ?? ''}"
-	data-gm-id={unitId}
-	data-gm-format={format}
-	data-gm-account-id={accountId ?? null}
-	data-gm-network={network ?? null}
+  class="gm-ad-unit {className ?? ''}"
+  data-gm-id={unitId}
+  data-gm-format={format}
+  data-gm-account-id={accountId ?? null}
+  data-gm-network={network ?? null}
+  href={undefined}
+  aria-label="GrowthMate Ad"
 ></a>
